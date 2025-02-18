95b02bc3e1b01b716ba36a26f2de52dc0ce7d06e
michal <michal.licht1806@gmail.com>
2025-02-18 19:57:20 +0100

    Change type of command position

--- lib/communication/mobile_communication.dart
+++ lib/communication/mobile_communication.dart
@@ -26,8 +26,8 @@ class MobileCommunication {
 
   /*
    * Commands:
-   * 2 characters - type of command
    * W - write / R - read
+   * 2 characters - type of command
    * data
    * k - end of command
    */
@@ -35,21 +35,21 @@ class MobileCommunication {
 
   /// Send status command. Server will respond with status data.
   static Future<void> sendStatusCommand() async {
-    await connection.sendCommand("STR", "");
+    await connection.sendCommand("RST", "");
   }
 
   /// Send brightness to the server.
   /// 
   /// param brightness - brightness value (0-100)
   static Future<void> sendStripBrightness(int brightness) async {
-    await connection.sendCommand("SBW", String.fromCharCode(brightness));
+    await connection.sendCommand("WSB", String.fromCharCode(brightness));
   }
 
   /// Send speed effect to the server.
   /// 
   /// param speed - speed value (0-100)
   static Future<void> sendStripSpeedEffect(int speed) async {
-    await connection.sendCommand("SSW", String.fromCharCode(speed));
+    await connection.sendCommand("WSS", String.fromCharCode(speed));
   }
 
   /// Send primary color to the server.
@@ -57,7 +57,7 @@ class MobileCommunication {
   /// @param color - color value
   static Future<void> sendPrimaryStripColor(Color color) async {
     // SCW {index} {red} {green} {blue} k
-    await connection.sendCommand("PCW", String.fromCharCode(0) + String.fromCharCode(color.red) + String.fromCharCode(color.green) + String.fromCharCode(color.blue));
+    await connection.sendCommand("WPC", String.fromCharCode(0) + String.fromCharCode(color.red) + String.fromCharCode(color.green) + String.fromCharCode(color.blue));
   }
 
   /// Send secondary color to the server.
@@ -65,13 +65,13 @@ class MobileCommunication {
   /// @param color - color value
   static Future<void> sendSecondaryStripColor(Color color) async {
     // SCW {index} {red} {green} {blue} k
-    await connection.sendCommand("SCW", String.fromCharCode(1) + String.fromCharCode(color.red) + String.fromCharCode(color.green) + String.fromCharCode(color.blue));
+    await connection.sendCommand("WSC", String.fromCharCode(1) + String.fromCharCode(color.red) + String.fromCharCode(color.green) + String.fromCharCode(color.blue));
   }
 
   /// Send effect to the server.
   /// 
   /// @param effect - effect index
   static Future<void> sendStripEffect(int effect) async {
-    await connection.sendCommand("SEW", String.fromCharCode(effect));
+    await connection.sendCommand("WSE", String.fromCharCode(effect));
   }
 }
